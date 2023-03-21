7a8b67793d04f291c7a5ce6894e64f97cd3f523b
Jose Luis Salazar <lsalazar@gmx.de>
2023-03-20 20:25:56 -0600

    Ajuste tamaño de ícono para marker

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -23,8 +23,12 @@ let searchLocation = reactive({
     zoom: 6,
 });
 const violetIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
 const currentVxFlagInfo = reactive({
     active: false,
