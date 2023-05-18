e5e54ae490959fa5e8f05f0b679c3010b9ff1336
Jose Luis Salazar <lsalazar@gmx.de>
2023-05-18 09:58:10 -0600

    Se implementó restricción de acceso según fecha de vigencia de suscripción

--- app/Http/Middleware/CheckUserHasAccess.php
+++ app/Http/Middleware/CheckUserHasAccess.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\URL;
@@ -20,15 +21,26 @@ class CheckUserHasAccess
     {
         if (!$request->user() ||
             (!$request->user()->active)) {
-            Auth::guard('web')->logout();
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
-
-            return $request->expectsJson()
-                ? abort(403, 'Cuenta desactivada.')
-                : \redirect()->back()->with('status', 'user-expired-deactivated');
+            return $this->handleInvalidAccount($request);
+        } else if ($request->user() && $request->user()->active && $request->user()->subscriber) {
+            $expirationDate =
+                Carbon::createFromFormat('Y-m-d', $request->user()->subscriber->expiration_date);
+            if (!Carbon::today()->lte($expirationDate)) {
+                return $this->handleInvalidAccount($request);
+            }
         }
 
         return $next($request);
+    }
+
+    private function handleInvalidAccount(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return $request->expectsJson()
+            ? abort(403, 'Cuenta desactivada o suscripción ha expirado.')
+            : \redirect()->back()->with('status', 'user-expired-deactivated');
     }
 }
