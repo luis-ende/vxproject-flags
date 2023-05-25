44a72fe494603dd5a052ee6f8955e9e158f279da
Jose Luis Salazar <lsalazar@gmx.de>
2023-05-25 16:40:35 -0600

    Cambios de textos de error de login en español

--- lang/es/auth.php
+++ lang/es/auth.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'username_failed' => 'El usuario con esta dirección no existe en nuestros registros.',
+    'password_failed' => 'Contraseña incorrecta.',
+    'failed'   => 'Usuario y/o contraseña no coinciden con nuestros registros.',
     'password' => 'La contraseña es incorrecta.',
     'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
 ];
