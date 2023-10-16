7ff97565ace7c250298a602a578c7c662a8549b3
Jose Luis Salazar <lsalazar@gmx.de>
2023-10-16 10:45:21 -0600

    Actualización de seeder de sitios del grupo temperaturas

--- database/seeders/VxFlagSeeder.php
+++ database/seeders/VxFlagSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Imports\VxFlagsImport;
 use App\Models\FlagGroupType;
+use App\Repositories\CatPaisesRepository;
 use Illuminate\Database\Seeder;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -14,13 +15,15 @@ class VxFlagSeeder extends Seeder
      */
     public function run(): void
     {
+        $catPaisesRepo = new CatPaisesRepository();
+
         $path = base_path('database/data/imports/TEMPERATURAS_2008.xlsx');
-        $import = new VxFlagsImport(1, FlagGroupType::TemperaturasCFE);
+        $import = new VxFlagsImport(1, FlagGroupType::TemperaturasCFE, $catPaisesRepo);
 
         Excel::import($import, $path);
 
         $path = base_path('database/data/imports/TEMPERATURAS_2020.xlsx');
-        $import = new VxFlagsImport(2, FlagGroupType::TemperaturasCFE);
+        $import = new VxFlagsImport(2, FlagGroupType::TemperaturasCFE, $catPaisesRepo);
 
         Excel::import($import, $path);
     }
